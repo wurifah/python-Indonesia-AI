de745dd2045747df35f7b50a70d616ce4aacc0be
wurifah <95538965+wurifah@users.noreply.github.com>
2021-12-16 00:01:44 +0700

    pertemuan 1 indonesia AI

---
+++ pertemuan1AI.py
@@ -0,0 +1,61 @@
+# y = 1945
+print("       ")
+print("   ==== y = 1945 ====    ")
+y = 1945;
+print("Data y = ",y," - tipe = ", type(y))
+data_string = str(y)
+data_float = float(y)
+data_bool = bool(y)
+print("Data string = ", data_string, ",type = ", type(data_string))
+print("Data float = ", data_float, ",type = ", type(data_float))
+print("Data boolean = ", data_bool, ",type = ", type(data_bool))
+
+
+# z = 71.80
+print("       ")
+print("   ==== z = 71.80 ====    ")
+z = 71.80;
+print("Data z = ",z," - tipe = ", type(z))
+data_int = int(z)
+data_string = str(z)
+data_bool = bool(z)
+print("Data integer = ", data_int, ",type = ", type(data_int))
+print("Data string = ", data_string, ",type = ", type(data_string))
+print("Data boolean = ", data_bool, ",type = ", type(data_bool))
+
+# m = true
+print("       ")
+print("   ==== m = true ====    ")
+m = True;
+print("Data m = ",m," - tipe = ", type(data_m))
+data_int = int(m)
+data_float = float(m)
+data_string = str(m)
+print("Data integer = ", data_int, ",type = ", type(data_int))
+print("Data float = ", data_float, ",type = ", type(data_float))
+print("Data string = ", data_string, ",type = ", type(data_string))
+
+# x = "I love AI"
+print("       ")
+print("   ==== n = 2*6/3 ====    ")
+n = 2*6/3
+print("n  = ",n," - tipe = ", type(n))
+data_int = int(n)
+data_float = float(n)
+data_bool = bool(n)
+print("Data integer = ", data_int, ",type = ", type(data_int))
+print("Data float = ", data_float, ",type = ", type(data_float))
+print("Data boolean = ", data_bool, ",type = ", type(data_bool))
+
+
+# x = "I love AI"
+print("       ")
+print("   =========    ")
+data_x = "I love AI";
+print("Data x = ",data_x," - tipe = ", type(data_x))
+data_int = int(data_x)
+data_float = float(data_x)
+data_bool = bool(data_x)
+print("Data integer = ", data_int, ",type = ", type(data_int))
+print("Data float = ", data_float, ",type = ", type(data_float))
+print("Data boolean = ", data_bool, ",type = ", type(data_bool))
